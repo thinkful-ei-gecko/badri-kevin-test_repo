9a46c165adb8197d7c5fec902e4ec4102ab9d710
Badri-narayan <badritulsiram@gmail.com>
2019-08-12 11:59:14 -0700

    TypeError

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ function createGreeting(name, age) {
   if (typeof name === 'undefined' || typeof age === 'undefined') {
     throw new Error('Arguments not valid.');
   }
+  if (typeof name !== 'string' || typeof age !== 'number'){
+    throw new TypeError();
+  }
 
   const yearOfBirth = getYearOfBirth(age);
   return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}`;
@@ -20,7 +23,7 @@ function getYearOfBirth(age){
 }
 
 try {
-  const greeting1 = createGreeting('Badri');
+  const greeting1 = createGreeting('Badri', 25);
   console.log(greeting1);
 } 
 catch (e){
