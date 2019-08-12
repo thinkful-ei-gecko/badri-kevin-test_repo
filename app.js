a8386f857ef747c3b33f69877b92c45a52e53c2a
Badri-narayan <badritulsiram@gmail.com>
2019-08-12 11:34:56 -0700

    added yearOfBirth

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ let myTeam = 'Badri and Kevin';
 console.log(myTeam);
 
 function createGreeting(name, age) {
-    return `Hi, my name is ${name} and I'm ${age} years old.`;
+  const yearOfBirth = 2019 - age;
+  return `Hi, my name is ${name} and I'm ${age} years old.
+     I was born in ${yearOfBirth}`;
 }
 
-const greeting1 = createGreeting("Badri", 25);
+const greeting1 = createGreeting('Badri', 25);
 console.log(greeting1);
