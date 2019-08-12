37cf018302fcb63cfa0b496547d2108892e52c02
Kevin Wei <chenghaowei@gmail.com>
2019-08-12 14:37:00 -0400

    add getYearOfBirth()

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ let myTeam = 'Badri and Kevin';
 console.log(myTeam);
 
 function createGreeting(name, age) {
-  const yearOfBirth = 2019 - age;
+  const yearOfBirth = getYearOfBirth(age);
   return `Hi, my name is ${name} and I'm ${age} years old.
      I was born in ${yearOfBirth}`;
+}
+
+function getYearOfBirth(age){
+  return 2019 - age;
 }
 
 const greeting1 = createGreeting('Badri', 25);
