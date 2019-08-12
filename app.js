88faf709e3eeee34e25a207fe20889742b706255
Kevin Wei <chenghaowei@gmail.com>
2019-08-12 14:51:13 -0400

    add name and age type checker

--- app.js
+++ app.js
@@ -3,24 +3,27 @@ let myTeam = 'Badri and Kevin';
 console.log(myTeam);
 
 function createGreeting(name, age) {
+  if (typeof name === 'undefined' || typeof age === 'undefined') {
+    throw new Error('Arguments not valid.');
+  }
+
   const yearOfBirth = getYearOfBirth(age);
-  return `Hi, my name is ${name} and I'm ${age} years old.
-     I was born in ${yearOfBirth}`;
+  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}`;
 }
 
 function getYearOfBirth(age){
   if (age < 0) {
     throw new Error ('Age cannot be negative');
   }
+
   return 2019 - age;
 }
 
 try {
-  const greeting1 = createGreeting('Badri', 25);
+  const greeting1 = createGreeting('Badri');
+  console.log(greeting1);
 } 
 catch (e){
   console.log(e);
 }
 
-const greeting1 = createGreeting('Badri', 25);
-console.log(greeting1);
