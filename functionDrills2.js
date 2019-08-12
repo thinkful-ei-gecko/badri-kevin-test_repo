c1f24a09b58be106c0855025ad90c0bde6c74984
Kevin Wei <chenghaowei@gmail.com>
2019-08-12 15:43:00 -0400

    decoder

--- functionDrills2.js
+++ functionDrills2.js
@@ -28,3 +28,32 @@ beyond(100);
 beyond(-100);
 beyond(0);
 */
+
+
+function decode (message) {
+  let msgArray = message.split(' ');
+  let decodedMsg = '';
+
+  for (let i = 0; i < msgArray.length; i++) {
+    if (msgArray[i][0] === 'a') {
+      decodedMsg += msgArray[i][1];
+    }
+    else if (msgArray[i][0] === 'b'){
+      decodedMsg += msgArray[i][2];
+    }
+    else if (msgArray[i][0] === 'c'){
+      decodedMsg += msgArray[i][3];
+    }
+    else if (msgArray[i][0] === 'd'){
+      decodedMsg += msgArray[i][4];
+    }
+    else {
+      decodedMsg += ' ';
+    }
+  }
+
+  return decodedMsg;
+}
+
+//console.log(decode('craft block argon meter bells brown croon droop'));
+//expected: for loop
