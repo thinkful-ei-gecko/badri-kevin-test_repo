7927ab7d07d01e09c27bdd12ec23dee6c323c26d
Badri-narayan <badritulsiram@gmail.com>
2019-08-12 12:16:47 -0700

    To infinity

--- functionDrills2.js
+++ functionDrills2.js
@@ -1,5 +1,30 @@
+'use strict';
+
 function jediName(firstName, lastName) {
-    return `${firstName.slice(0, 3)}${lastName.slice(0, 2)}`;
+  return `${firstName.slice(0, 3)}${lastName.slice(0, 2)}`;
 }
 
-console.log(jediName('Badri', 'Tulsiram'));
+//console.log(jediName('Badri', 'Tulsiram'));
+
+function beyond(num){
+  if (num === Number.POSITIVE_INFINITY || num === Number.NEGATIVE_INFINITY) {
+    console.log('And beyond');
+  }
+  else if (num > 0){
+    console.log('To infinity');
+  }
+  else if (num < 0){
+    console.log('To negative infinity');
+  } 
+  else {
+    console.log('Staying home');
+  }
+}
+
+/*
+beyond(Number.POSITIVE_INFINITY);
+beyond(Number.NEGATIVE_INFINITY);
+beyond(100);
+beyond(-100);
+beyond(0);
+*/
